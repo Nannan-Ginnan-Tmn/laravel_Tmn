055cd02024b3576372f509e99d3cf95747018b08
Nannan-Ginnan-Tmn <matsu1341@gmail.com>
2019-01-31 16:02:01 +0900

    task migration

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
 
+/**
+ * 全タスク表示
+ */
 Route::get('/', function () {
-    return view('welcome');
+    //
 });
+
+/**
+ * 新タスク追加
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * 既存タスク削除
+ */
+Route::delete('/task/{id}', function ($id) {
+    //
+});
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
